92b17fe3f9ff393b2de802c379717bea70e74599
creed6312 <creed.shane@gmail.com>
2016-08-16 11:56:00 +0200

    IBaseActivity Rename

---
+++ app/src/main/java/com/example/shanesardinha/codeproject/Interfaces/IBaseActivity.java
@@ -0,0 +1,12 @@
+package com.example.shanesardinha.codeproject.Interfaces;
+
+/**
+ * Created by shanesardinha on 2016/08/10.
+ */
+public interface IBaseActivity {
+
+     void setUpViews();
+
+     void createPresenter();
+ 
+ }
